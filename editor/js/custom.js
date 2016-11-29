2766ff792ee2eb68ec865e1bbcc7873e86cdbc29
vihanga-liyanage <vihangaliyanage007@gmail.com>
2016-11-29 14:39:41 +0530

    JSON parser added

--- editor/js/custom.js
+++ editor/js/custom.js
@@ -1,186 +1,3 @@
-/**
- * Created by wso2123 on 11/25/16.
- */
-
-
-var showComponentInfo  =  function(state,evt) {
-    // console.log(state.style.image)
-    // console.log(evt);
-
-    var component = getImageNameFromPath(state.style.image);
-    $("#slide_header").text(product_details[component].title);
-    $('#slide_img').attr("src","./images/wso2/" + component + ".png");
-    $("#slide_content").html("Loading");
-
-    getSuggests(component,'popup');
-
-    $('#slide').popup({
-        outline: true, // optional
-        focusdelay: 400, // optional
-        vertical: 'center', //optional
-        autoopen:true,
-
-    });
-};
-
-var setDetails = function(component, type){
-
-    // console.log("setDetails-" + component + '-' + type);
-    $('#detail_view_img').attr("src","./images/wso2/" + component + ".png");
-    $('#detail_view_title').text(product_details[component].title)
-    if(type === 'toolbox'){;
-        $('#detail_view_content').html(product_details[component].description);
-        getSuggests(component, 'toolbox');
-    }
-    else if(type==='graph'){
-        $('#detail_view_content').html("Here are some suggestions to connect "+product_details[component].title +".<br/><br/>")
-        $('#links_details').html("");
-        var suggestListContent = "<ul class='list-group'>"
-        product_suggestions[component].forEach( function (item) {
-            suggestListContent+=generateSuggestions(item);
-        });
-        suggestListContent+="</ul>"
-        $('#detail_view_content').append(suggestListContent);
-    }
-}
-
-var generateSuggestions = function (item) {
-    var component = item.component;
-    return "<li class='list-group-item'>" +
-        "<table><tr><td style='width: 80px;'><img class='suggest_img'  src='"+"./images/wso2/"+component+".png' /></td>" +
-        "<td><b> " +product_details[component].title+  "</b>" +
-        "<p>" +item.description+
-        "</p></td></tr></table> </li>";
-
-}
-
-var getImageNameFromPath = function(path){
-    var name = path.split('/')
-    name = name[name.length-1]
-    return name.substring(0,name.length-4).toLowerCase();
-}
-
-setTimeout(function () {
-    $('img').on( "click", function (evt) {
-        // console.log( 'click');
-        setDetails(getImageNameFromPath(evt.target.src), 'toolbox');
-    } );
-} ,1000);
-
-// Mouse Move ----------------------------------------------------------------
-var addListeners = function (editor) {
-    var graph = editor.graph
-    graph.addMouseListener(
-    {
-        currentState: null,
-        previousStyle: null,
-        mouseDown: function(sender, me) {
-            // console.log('mouseDown');
-        },
-        mouseMove: function(sender, me) {
-            // console.log('mouseMove');
-        },
-        mouseUp: function(sender, me) {
-            // console.log('mouseup');
-            if (me.state != null) {
-                setDetails(me.state.cell.getStyle(), 'graph');
-            }
-            //listener for click on edges
-            if (me.sourceState != null && me.sourceState.cell.isEdge() == true) {
-                // console.log("clicked on edge");
-            }
-
-        },
-        dragEnter: function(evt, state) {
-            // console.log('dragEnter');
-        },
-        dragLeave: function(evt, state) {
-            // console.log('dragLeave');
-        }
-    });
-}
-
-var getSuggests = function (component, place) {
-    $.getJSON( "http://10.100.4.196:5000/article_suggest/"+component, function( data ) {
-        // $( ".result" ).html( data );
-        // if(Object.keys(data))
-        var txtSlideContent = "<ul>";
-        Object.keys(data).forEach(function (key) {
-            txtSlideContent+="<li><a target='_blank' href='"+data[key]+"'>"+key+"</a></li>"
-        });
-        txtSlideContent+="</ul>"
-        $("#slide_content").html(txtSlideContent);
-        if(place=='toolbox')
-            $('#links_details').html(txtSlideContent);
-        else
-            $('#links_details').html("");
-        console.log(data )
-    });
-}
-
-$(document).ready(function() {
-
-    // Initialize the plugin
-    $('#contact_popup').popup();
-
-    $('.contact_popup_open').on('click', function (evt) {
-
-        // var canvas = document.getElementById("#drawing_pad");
-        // var img = canvas.toDataUR    L("image/png");
-        $('#contact_popup_img').html("")
-        var svg = $("svg").clone();
-        $('#contact_popup_img').html(svg );
-    })
-
-    $('#final_form_submit').on('click', function (event) {
-        event.preventDefault();
-        var formData = {
-            'name'              : $('#exampleName').val(),
-            'email'             : $('#exampleInputEmail1').val(),
-            'superheroAlias'    : $('input[name=superheroAlias]').val()
-        };
-
-        var rotiData = {
-            "event": {
-                "payloadData": {
-                    "FirstName": $('#exampleName').val(),
-                    "LastName": $('#exampleLast').val(),
-                    "Email": $('#exampleInputEmail').val(),
-                    "Title": $('#exampleJob').val(),
-                    "Company": $('#exampleCompany').val(),
-                    "Country": $('#exampleCountry').val(),
-                    "Ipaddress": "199.119.233.228",
-                    "Region": "ROW"
-                }
-            }
-        };
-
-        // process the form
-        $.ajax({
-            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-            url         : 'http://10.100.4.196:9763/endpoints/input_listner', // the url where we want to POST
-            data        : rotiData, // our data object
-            dataType    : 'json', // what type of data do we expect back from the server
-            encode          : true
-        })
-        // using the done promise callback
-        .done(function(data) {
-
-            // log data to the console so we can see
-            console.log(data);
-
-            // here we will handle errors and validation messages
-        });
-
-        setTimeout(function () {
-            $('#contact_popup').popup( 'hide');
-            $('#success_popup').popup('show');
-            setTimeout(function () {
-                $('#success_popup').popup('hide');
-            }, 1000)
-        }, 500);
-    })
-});
 
 var generateGraph = function (configXML, editor) {
     var graphNode = editor.graph.container;
@@ -195,4 +12,86 @@ var generateGraph = function (configXML, editor) {
     {
         mxUtils.clearSelection();
     }
+};
+
+var jsonText = '{"services":[' +
+'{' +
+    '"id":1,' +
+    '"type":"database",' +
+    '"name":"apim_rdbms",' +
+    '"image":"mysql:5.5",' +
+    '"ports":[],' +
+    '"links":[]' +
+'},' +
+'{' +
+    '"id":2,' +
+    '"type":"wso2am",' +
+    '"name":"api-manager",' +
+    '"image":"docker.wso2.com/wso2am:2.0.0",' +
+    '"ports":[' +
+    '"443:9443",' +
+    '"80:9763",' +
+    '"8280:8280",' +
+    '"8243:8243"' +
+'],' +
+    '"links":[' +
+    '{' +
+        '"service-id":1,' +
+        '"type":"database",' +
+        '"port":"3306"' +
+    '}' +
+']' +
+'},' +
+'{' +
+    '"id":3,' +
+    '"type":"wso2am-analytics",' +
+    '"name":"am-analytics",' +
+    '"image":"docker.wso2.com/wso2am-analytics:2.0.0",' +
+    '"ports":[' +
+    '"9444:9444",' +
+    '"9764:9764"' +
+'],' +
+    '"links":[' +
+    '{' +
+        '"service-id":1,' +
+        '"type":"database",' +
+        '"port":"3306"' +
+    '}' +
+']' +
+'}' +
+']}';
+
+// console.log(JSON.parse(jsonText));
+
+//function to load json files
+function loadJSON(path, callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', path, true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+//reads the model.json and generate xml config file
+var generateConfigXML = function (jsonModelPath) {
+    console.log(jsonModelPath + "-->");
+
+    //JQuery jason load
+    // $.getJSON(jsonModelPath, function(json) {
+    //     var services = json.services;
+    //     for (service in services) {
+    //         console.log(services[service].type);
+    //     }
+    // }).fail( function(d, textStatus, error) {
+    //     console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+    // });
+
+
+
 }
