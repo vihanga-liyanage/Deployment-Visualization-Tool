b386921a8c242773353d82c701be5119f51ca288
vihanga-liyanage <vihangaliyanage007@gmail.com>
2016-12-09 12:53:42 +0530

    Added testForwardLinksWithProfiles()

--- docker-compose-gen/src/main/java/org/wso2/deployment/Generate.java
+++ docker-compose-gen/src/main/java/org/wso2/deployment/Generate.java
@@ -46,15 +46,55 @@ public class Generate {
                 int targetID = link.getInt("target");
 
                 if (sourceID == serviceID) {
+                    //links start from this service
                     JSONObject linkedService = services.getJSONObject(targetID);
+                    String linkedType = getType(linkedService);
+
+                    JSONArray linkedServiceProfiles = linkedService.optJSONArray("profiles");
+                    if (profiles.length() > 1) {
+                        //If source service had more than one profile
+                        for (int p = 0; p < profiles.length(); p++) {
+                            serviceName = withProfile(type, profiles.getString(p));
+
+                            if (linkedServiceProfiles != null) {
+                                if (linkedServiceProfiles.length() == 0) {
+                                    fileNames.add(serviceName + "," + linkedType);
+                                } else {
+                                    for (int k = 0; k < linkedServiceProfiles.length(); k++) {
+                                        fileNames.add(serviceName + "," + withProfile(linkedType, linkedServiceProfiles.getString(k)));
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        if (linkedServiceProfiles != null) {
+                            if (linkedServiceProfiles.length() == 0) {
+                                fileNames.add(serviceName + "," + linkedType);
+                            } else {
+                                for (int k = 0; k < linkedServiceProfiles.length(); k++) {
+                                    fileNames.add(serviceName + "," + withProfile(linkedType, linkedServiceProfiles.getString(k)));
+                                }
+                            }
+                        }
+                    }
+
+
+
+                }
+
+                if (targetID == serviceID) {
+                    //links end from this service
+                    JSONObject linkedService = services.getJSONObject(sourceID);
                     String linkedType = getType(linkedService);
 
                     JSONArray linkedServiceProfiles = linkedService.optJSONArray("profiles");
                     if (linkedServiceProfiles != null) {
                         if (linkedServiceProfiles.length() == 0) {
                             fileNames.add(serviceName + "," + linkedType);
-                        } else if (linkedServiceProfiles.length() == 1) {
-                            fileNames.add(serviceName + "," + withProfile(linkedType, linkedServiceProfiles.getString(0)));
+                        } else {
+                            for (int k = 0; k < linkedServiceProfiles.length(); k++) {
+                                fileNames.add(serviceName + "," + withProfile(linkedType, linkedServiceProfiles.getString(k)));
+                            }
                         }
                     }
                 }
--- docker-compose-gen/src/test/java/org/wso2/deployment/GenerateTest.java
+++ docker-compose-gen/src/test/java/org/wso2/deployment/GenerateTest.java
@@ -8,6 +8,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.Collections.singletonList;
+
 public class GenerateTest {
 
 //    @Test(expectedExceptions = Exception.class)
@@ -19,7 +21,7 @@ public class GenerateTest {
     public void testSimpleProduct() throws Exception {
         String model = "{\"services\":[{\"type\":\"database\",\"profiles\":[]}],\"links\":[]}";
         List<String> dirNames = Generate.toKnowledgeBaseNames(0, new JSONObject(model));
-        Assert.assertEquals(dirNames, Collections.singletonList("database"));
+        Assert.assertEquals(dirNames, singletonList("database"));
     }
 
     @Test
@@ -27,7 +29,7 @@ public class GenerateTest {
         String model = "{\"services\":[{\"type\":\"database\",\"profiles\":[]},{\"type\":\"wso2am\",\"profiles\""
                 + ":[\"publisher\"]},{\"type\":\"wso2am\",\"profiles\":[\"store\"]}],\"links\":[]}";
         List<String> dirNames = Generate.toKnowledgeBaseNames(2, new JSONObject(model));
-        Assert.assertEquals(dirNames, Collections.singletonList("wso2am_store"));
+        Assert.assertEquals(dirNames, singletonList("wso2am_store"));
     }
 
 //    @Test
@@ -61,12 +63,13 @@ public class GenerateTest {
         String model = "{\"services\":[{\"type\":\"database\",\"profiles\":[]},{\"type\":\"wso2am\",\"profiles\":[\""
                 + "publisher\"]}],\"links\":[{\"source\":0,\"target\":1}]}";
         JSONObject modelJson = new JSONObject(model);
+        List<String> dirNames;
 
-        List<String> dirNames = Generate.toKnowledgeBaseNames(0, modelJson);
-        Assert.assertEquals(dirNames, Arrays.asList("database", "database,wso2am_publisher"));
+//        dirNames = Generate.toKnowledgeBaseNames(0, modelJson);
+//        Assert.assertEquals(dirNames, Arrays.asList("database", "database,wso2am_publisher"));
 
         dirNames = Generate.toKnowledgeBaseNames(1, modelJson);
-        Assert.assertEquals(dirNames, Arrays.asList("wso2am_publisher"));
+        Assert.assertEquals(dirNames, Arrays.asList("wso2am_publisher", "wso2am_publisher,database"));
     }
 
     @Test
@@ -75,12 +78,45 @@ public class GenerateTest {
                 + "publisher\"]},{\"type\":\"wso2am\",\"profiles\":[\"store\"]}],\"links\":[{\"source\":0,\""
                 + "target\":1},{\"source\":0,\"target\":2},{\"source\":1,\"target\":2}]}";
         JSONObject modelJson = new JSONObject(model);
+        List<String> dirNames;
 
-        List<String> dirNames = Generate.toKnowledgeBaseNames(0, modelJson);
+        dirNames = Generate.toKnowledgeBaseNames(0, modelJson);
         Assert.assertEquals(dirNames, Arrays.asList("database", "database,wso2am_publisher", "database,wso2am_store"));
 
         dirNames = Generate.toKnowledgeBaseNames(1, modelJson);
-        Assert.assertEquals(dirNames, Arrays.asList("wso2am_publisher", "wso2am_publisher,wso2am_store"));
+        Assert.assertEquals(dirNames, Arrays.asList("wso2am_publisher", "wso2am_publisher,database", "wso2am_publisher,wso2am_store"));
     }
 
+    @Test
+    public void testLinkWithProfiles() throws Exception {
+        String model = "{\"services\":[{\"type\":\"database\",\"profiles\":[]},{\"type\":\"wso2am\",\"profiles\""
+                + ":[\"publisher\",\"store\"]}],\"links\":[{\"source\":0,\"target\":1}]}";
+        JSONObject modelJson = new JSONObject(model);
+        List<String> dirNames;
+
+        dirNames = Generate.toKnowledgeBaseNames(0, modelJson);
+        Assert.assertEquals(dirNames, Arrays.asList("database", "database,wso2am_publisher", "database,wso2am_store"));
+    }
+
+    @Test
+    public void testForwardLinksWithProfiles() throws Exception {
+        String model = "{\"services\":[{\"type\":\"wso2am\",\"profiles\":[\"publisher\",\"store\"]},{\"type\":\"wso2"
+                + "am\",\"profiles\":[\"keymanager\",\"traffic-manager\"]}],\"links\":[{\"source\":0,\"target\":1}]}";
+        JSONObject modelJson = new JSONObject(model);
+        List<String> dirNames;
+
+        dirNames = Generate.toKnowledgeBaseNames(0, modelJson);
+        Assert.assertEquals(dirNames, Arrays.asList("wso2am_publisher,wso2am_keymanager", "wso2am_publisher,wso2am_traffic-manager", "wso2am_store,wso2am_keymanager", "wso2am_store,wso2am_traffic-manager"));
+    }
+
+    @Test
+    public void testBackwardLinksWithProfiles() throws Exception {
+        String model = "{\"services\":[{\"type\":\"wso2am\",\"profiles\":[\"publisher\",\"store\"]},{\"type\":\"wso2"
+                + "am\",\"profiles\":[\"keymanager\",\"traffic-manager\"]}],\"links\":[{\"source\":0,\"target\":1}]}";
+        JSONObject modelJson = new JSONObject(model);
+        List<String> dirNames;
+
+        dirNames = Generate.toKnowledgeBaseNames(1, modelJson);
+        Assert.assertEquals(dirNames, Arrays.asList("wso2am_keymanager,wso2am_publisher", "wso2am_keymanager,wso2am_store", "wso2am_traffic-manager,wso2am_publisher", "wso2am_traffic-manager,wso2am_store"));
+    }
 }
